f5a868f237555fe176477a24f28feca3fbe0bfce
hannenule <hannenule@outlook.com>
2018-11-28 09:24:53 -0500

    Empezando

---
+++ Hanne/Main.py
@@ -0,0 +1,11 @@
+# coding: utf8
+import sys
+
+# Procedimiento principal, punto de inicio de la aplicacion
+def Main(args):
+	print("Hola Mundo")
+
+
+# Inicio de la aplicacion. NoTocar.
+if __name__ == '__main__':
+	Main(sys.argv)
